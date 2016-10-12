96afae2e6279bd893fc426596531e7be122d1e89
Sarath VS <sarathvss65@gmail.com>
2016-10-12 19:07:50 +0530

    #01 Sarath: Annotate EventsController with RequestBody

--- src/main/java/com/adenlie/adventure/controller/EventsController.java
+++ src/main/java/com/adenlie/adventure/controller/EventsController.java
@@ -2,11 +2,13 @@ package com.adenlie.adventure.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 @RequestMapping("/events")
+@ResponseBody
 public class EventsController {
 
     @RequestMapping(value = "/all.json", method = GET)

d930d06a3589e3b1c3cbbe01f091ec341d5a5636
Sarath VS <sarathvss65@gmail.com>
2017-01-17 19:28:03 +0530

    #01 Sarath: Fixing migration for Events table creation

--- src/main/java/db/migration/db.migration.V1__Create_events_table.sql
+++ src/main/java/db/migration/db.migration.V1__Create_events_table.sql
@@ -1,1 +1,8 @@
-Cre
+CREATE TABLE events (
+    event_id VARCHAR(20) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    start_date DATE,
+    end_date DATE,
+    duration INTEGER
+    cost NUMERIC
+);
